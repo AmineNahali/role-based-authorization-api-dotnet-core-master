1ccbfe52d82c678068e0422b8f33c16319b43dc8
Amine <nahali.amine.32@gmail.com>
2024-10-18 21:54:18 +0100

    jwt & roles

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,26 @@
+namespace WebApi.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    private readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //options.UseInMemoryDatabase("TestDb");
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), (sqlOptions) =>
+            {
+                sqlOptions.EnableRetryOnFailure();
+            }
+        );
+    }
+}
